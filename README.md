8eed672ce65cc6ffd07a345f6d8e8e42118179b5
thaisdev <thaishdzlpz@gmail.com>
2019-10-09 14:07:15 +0100

    added stats in js, added responsive for screen between 320px & 500px

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# PokeFetch
+
+PokeFetch es una página web que mediante consultas a la API de [Pokemon](https://pokeapi.co/) me permite mejorar mis habilidades en :
+
+* HTML
+* CSS
+* Javascript 
+
+
+![logotipo-pokemon](https://cutt.ly/qein45l)
+
+
--- app.js
+++ app.js
@@ -8,12 +8,24 @@ searchButton.addEventListener('click', searchPokemon);
 function searchPokemon() {
 
     const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
-    const textSearchInput = document.querySelector('#js__input-search').value;
+    const textSearchInput = document.querySelector('#js__input__search').value;
     
     const pokemon = `${BASE_URL}${textSearchInput}`;
+    const stats = [];
     
-    axios.get(pokemon)
+    axios.get("https://pokeapi.co/api/v2/pokemon/150")
         .then(pokemonJSON => {
+            stats.push({
+                name: pokemonJSON.data.name,
+                type: pokemonJSON.data.types[0].type.name,
+                speed: pokemonJSON.data.stats[0].base_stat,
+                "special-defense": pokemonJSON.data.stats[1].base_stat,
+                "special-attack" : pokemonJSON.data.stats[2].base_stat,
+                defense : pokemonJSON.data.stats[3].base_stat,
+                attack : pokemonJSON.data.stats[4].base_stat,
+                hp : pokemonJSON.data.stats[5].base_stat,
+            })
+            renderStats(stats);
             return renderPokemon(pokemonJSON.data.sprites.front_default);
         })
         .catch(() => { 
@@ -22,16 +34,32 @@ function searchPokemon() {
 }
 
 
-function renderPokemon(sprite) {
+function renderPokemon(sprite,stats) {
 
+    
     messageError.textContent = '';
-
+    
     pokemon.setAttribute('src', sprite);
-    pokemon.style.display = 'inherit';
 }
 
 function renderMessageError() {
 
     pokemon.style.display = 'none';
     messageError.textContent = 'Ningún Pokémon Salvaje Apareció 😥';
+}
+
+
+function renderStats(stats) {
+    
+    const statsPokemon = document.querySelector("#stats-pokemon");
+    const HP = document.querySelector("#hp");
+    const nameType = document.querySelector("#name__type");
+    statsPokemon.classList.remove("hidden");
+
+    stats.map( attribute => {
+
+        HP.textContent = `HP: ${attribute.hp}`;
+        nameType.textContent = `${attribute.name} ${attribute.type}`
+    });
+
 }
